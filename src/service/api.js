20d6919ff7ed4d3ede40b7db80673e47931e4326
Collins-peter <anicollins09@gmail.com>
2026-01-09 15:37:49 +0100

    .env VITE_API update

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 //CREATE AXIOS BASE CONFIGURATION
 const api = axios.create({
-    baseURL: import.meta.env.API_BACKEND_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type" : "application/json"
     },
